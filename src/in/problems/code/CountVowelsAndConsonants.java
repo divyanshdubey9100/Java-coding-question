7955d3201b0edb0797d4b6b311977b0761225e6b
divyanshdubey9100 <divyanshdubey9100@gmail.com>
2025-11-16 14:01:59 +0530

    1. Reverse a string 2. Check palindrome number 3. Find factorial using recursion 4. Check prime number 5. Generate Fibonacci series 6. Find largest and smallest in array 7. Reverse an array 8. Find duplicate elements in array 9. Remove duplicates from array 10. Sort array without built-in methods 11. Find missing number in array 12. Count vowels and consonants 13. Check anagram strings 14. Count occurrences of characters 15. Find first non-repeated character 16. Swap two numbers without temp variable 17. Reverse words in sentence 18. Convert decimal to binary

---
+++ src/in/problems/code/CountVowelsAndConsonants.java
@@ -0,0 +1,29 @@
+package in.problems.code;
+
+import in.problems.code.commons.UserInput;
+
+public class CountVowelsAndConsonants {
+	
+	public static int[] countVowelsAndConsonants(String str) {
+		int vowelsCount=0,consonantCount=0;
+		String vowels="aeiouAEIOU";
+		for(int i=0;i<str.length();i++) {
+			char ch=str.charAt(i);
+			if(Character.isLetter(ch)) {
+				if(vowels.indexOf(ch) !=-1) {
+					vowelsCount++;
+				}else {
+					consonantCount ++;
+				}
+			}
+		}
+		return new int[] {vowelsCount,consonantCount};
+	}
+
+	public static void main(String[] args) {
+		int[] result=countVowelsAndConsonants(UserInput.getStringSentenceInput());
+		System.out.println("Vowels :"+result[0]+" Consonants :"+result[1]);
+
+	}
+
+}
